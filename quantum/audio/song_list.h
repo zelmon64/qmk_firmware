0bd832b2135c38f209713fa0329f3b6de2456fc4
zelmon64 <zelmon64@gmail.com>
2017-05-31 22:14:15 +0100

    Minor UK ISO modification with Zelda music

--- quantum/audio/song_list.h
+++ quantum/audio/song_list.h
@@ -167,13 +167,68 @@
     HD_NOTE(_C7),
 
 #define ZELDA_PUZZLE \
-    Q__NOTE(_G5),     \
-    Q__NOTE(_FS5),    \
-    Q__NOTE(_DS5),     \
+    Q__NOTE(_G5),    \
+    Q__NOTE(_FS5),   \
+    Q__NOTE(_DS5),   \
     Q__NOTE(_A4),    \
-    Q__NOTE(_GS4),     \
-    Q__NOTE(_E5),     \
-    Q__NOTE(_GS5),     \
+    Q__NOTE(_GS4),   \
+    Q__NOTE(_E5),    \
+    Q__NOTE(_GS5),   \
     HD_NOTE(_C6),
+
+#define ZELDA_CHEST \
+    Q__NOTE(_G5),   \
+    Q__NOTE(_A5),   \
+    Q__NOTE(_B5),   \
+    Q__NOTE(_CS6),  \
+    Q__NOTE(_G5),   \
+    Q__NOTE(_A5),   \
+    Q__NOTE(_B5),   \
+    Q__NOTE(_CS6),  \
+    Q__NOTE(_GS5),  \
+    Q__NOTE(_AS5),  \
+    Q__NOTE(_C6),   \
+    Q__NOTE(_D6),   \
+    Q__NOTE(_GS5),  \
+    Q__NOTE(_AS5),  \
+    Q__NOTE(_C6),   \
+    Q__NOTE(_D6),   \
+    Q__NOTE(_A5),   \
+    Q__NOTE(_B5),   \
+    Q__NOTE(_CS6),  \
+    Q__NOTE(_DS6),  \
+    Q__NOTE(_A5),   \
+    Q__NOTE(_B5),   \
+    Q__NOTE(_CS6),  \
+    Q__NOTE(_DS6),  \
+    Q__NOTE(_AS5),  \
+    Q__NOTE(_C6),   \
+    Q__NOTE(_D6),   \
+    Q__NOTE(_E6),   \
+    Q__NOTE(_AS5),  \
+    Q__NOTE(_C6),   \
+    Q__NOTE(_D6),   \
+    Q__NOTE(_E6),   \
+    Q__NOTE(_B5),   \
+    Q__NOTE(_CS6),  \
+    Q__NOTE(_DS6),  \
+    Q__NOTE(_F6),   \
+    Q__NOTE(_C6),   \
+    Q__NOTE(_D6),   \
+    Q__NOTE(_E6),   \
+    Q__NOTE(_FS6),  \
+    Q__NOTE(_CS6),  \
+    Q__NOTE(_DS6),  \
+    Q__NOTE(_F6),   \
+    Q__NOTE(_G6),   \
+    Q__NOTE(_D6),   \
+    Q__NOTE(_E6),   \
+    Q__NOTE(_FS6),  \
+    Q__NOTE(_GS6),  \
+    S__NOTE(_REST), \
+    Q__NOTE(_A6),   \
+    Q__NOTE(_AS6),  \
+    Q__NOTE(_B6),   \
+    HD_NOTE(_C7),
 
 #endif
