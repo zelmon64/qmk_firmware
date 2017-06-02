521eea7fc631fffb4dcdcbdf60a92bfb8fb1f74d
zelmon64 <zelmon64@gmail.com>
2017-06-02 10:46:34 +0100

    Zelda Item_Get

--- keyboards/planck/keymaps/zelmon64/keymap.c
+++ keyboards/planck/keymaps/zelmon64/keymap.c
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   F  |   P  |   B  |   J  |   L  |   U  |   Y  |   ;  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   R  |   S  |   T  |   G  |   K  |   N  |   E  |   I  |   O  |  '   |
+ * | Esc  |   A  |   R  |   S  |   T  |   G  |   K  |   N  |   E  |   I  |   O  |   '  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   D  |   V  |   M  |   H  |   ,  |   .  |  Up  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -96,46 +96,46 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_CMODDH] = {
-  {KC_TAB,         KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC       },
-  {KC_ESC,         KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_K,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT       },
-  {LSFT_T(KC_DEL), KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_M,    KC_H,    KC_COMM, KC_DOT,  KC_UP,   RSFT_T(KC_ENT)},
-  {MOUSE,          KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT       }
+  {KC_TAB,         KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,            KC_Y,    KC_SCLN, KC_BSPC       },
+  {KC_ESC,         KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_K,    KC_N,    KC_E,            KC_I,    KC_O,    KC_QUOT       },
+  {LSFT_T(KC_DEL), KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_M,    KC_H,    KC_COMM,         KC_DOT,  KC_UP,   RSFT_T(KC_ENT)},
+  {MOUSE,          KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   RCTL_T(KC_SLSH), KC_LEFT, KC_DOWN, KC_RGHT       }
 },
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |   ¬  |   !  |   "  |   £  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
+ * |  ¬   |   !  |   "  |   £  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  ~   |
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |   ~  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |      | Vol+ | Play |
+ * | Bksp |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |      | Vol+ | Play |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |   |  | Prev | Vol- | Next |
+ * |      |      |      |      |      |     Tab     |      |   |  | Prev | Vol- | Next |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
   {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,    KC_LPRN, KC_RPRN, KC_DEL },
   {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,    _______, KC_VOLU, KC_MPLY},
-  {_______, _______, _______, _______, _______, _______, _______, _______, S(KC_NUBS), KC_MPRV, KC_VOLD, KC_MNXT}
+  {KC_BSPC, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,    _______, KC_VOLU, KC_MPLY},
+  {_______, _______, _______, _______, _______, KC_TAB,  KC_TAB,  _______, S(KC_NUBS), KC_MPRV, KC_VOLD, KC_MNXT}
 },
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
+ * |  `   |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  #   |
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |   #  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |      | Pg Up| Mute |
+ * | Bksp |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |      | Pg Up| Mute |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |   \  | Home | Pg Dn| End  |
+ * |      |      |      |      |      |     Tab     |      |   \  | Home | Pg Dn| End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = {
   { KC_GRV, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL },
   {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, KC_PGUP, KC_MUTE},
-  {_______, _______, _______, _______, _______, _______, _______, _______, KC_NUBS, KC_HOME, KC_PGDN, KC_END }
+  {KC_BSPC, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, KC_PGUP, KC_MUTE},
+  {_______, _______, _______, _______, _______, KC_TAB,  KC_TAB,  _______, KC_NUBS, KC_HOME, KC_PGDN, KC_END }
 },
 
 /* Mouse and Number layer
@@ -146,7 +146,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |   *  |   1  |   2  |   3  |   +  |      |      |      |      | L_Clk|   ↑  | R_Clk|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |CModDH|   0  |   0  |   .  |      |    Enter    |      |      |   ←  |   ↓  |   →  |
+ * |CModDH|   0  |   0  |   .  |      |    Enter    |      | Bksp |   ←  |   ↓  |   →  |
  * `-----------------------------------------------------------------------------------'
  */
 
@@ -154,7 +154,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_NLCK, KC_P7, KC_P8, KC_P9,   S(KC_6), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_U, XXXXXXX, KC_WH_D},
   {KC_PSLS, KC_P4, KC_P5, KC_P6,   KC_PMNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN4, KC_BTN3, KC_BTN5},
   {KC_PAST, KC_P1, KC_P2, KC_P3,   KC_PPLS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN1, KC_MS_U, KC_BTN2},
-  {CMODDH,  KC_P0, KC_P0, KC_PDOT, _______, KC_PENT, KC_PENT, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R}
+  {CMODDH,  KC_P0, KC_P0, KC_PDOT, _______, KC_PENT, KC_PENT, _______, KC_BSPC, KC_MS_L, KC_MS_D, KC_MS_R}
 },
 
 /* Adjust (Lower + Raise)
@@ -183,7 +183,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 float tone_startup[][2]    = SONG(ZELDA_CHEST);
 float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
 float tone_colemakdh[][2]  = SONG(ZELDA_PUZZLE);
-float tone_mouse[][2]      = SONG(SONIC_RING);
+float tone_mouse[][2]      = SONG(ZELDA_ITEM);
 float music_scale[][2]     = SONG(MUSIC_SCALE_SOUND);
 
 float tone_goodbye[][2] =    SONG(GOODBYE_SOUND);
